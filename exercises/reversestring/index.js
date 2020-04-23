dd850d8ab8824196d38366e38c29e74e2cc26c6a
Tosh Velaga <toshvelaga@gmail.com>
2020-04-22 22:43:27 -0700

    did reversestring ex

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,26 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    debugger;
+    return str.split('').reduce((rev, char) => char + rev, '');
+}
 
 module.exports = reverse;
+
+// function reverse(str) {
+//     var string = str.split("")
+//     var reversedArray = string.reverse();
+//     var joined = reversedArray.join("")
+//     return joined;
+// }
+
+// function reverse(str) {
+//     let reversed = '';
+
+//     for (let character of str) {
+//         reversed = character + reversed;
+//         console.log(reversed)
+//     }
+//     return reversed;
+// }
